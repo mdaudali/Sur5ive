942441fd146d8e3f33201c74dc6753b1d7b98bdd
Ben Smith <besmith166@gmail.com>
2015-05-26 13:35:46 +0100

    funny bug

--- game.py
+++ game.py
@@ -2,17 +2,18 @@ __author__ = "ben"
 
 import pygame, pygame.gfxdraw
 size = 800, 600
-
+x,y = 300,400
+angle = 10
 GREEN         = (   0, 255,   0)
 RED           = ( 255,   0,   0)
 
-class Shuttle(pygame.sprite.Sprite):
-    def __init__(self):
-        self.image = pygame.image.load("1.png")
-        self.x = 270
-        self.y = 400
-        self.rect = self.image.get_rect()
-class Player(pygame.sprite.Sprite): #changes the image and can be moved. Use if you want.
+#class Shuttle(pygame.sprite.Sprite):
+#    def __init__(self):
+#        self.image = pygame.image.load("1.png")
+ #       self.x = 270
+ #       self.y = 400
+ #       self.rect = self.image.get_rect()
+class Player(pygame.sprite.Sprite):  # changes the image and can be moved. Use if you want.
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.state = 1
@@ -21,6 +22,7 @@ class Player(pygame.sprite.Sprite): #changes the image and can be moved. Use if 
         self.state = 1
         self.no = 0
         self.x = 0
+        self.dir = 0
     def update(self, pressed):
         self.no = self.no + 1
         if self.no <= 2:
@@ -42,31 +44,52 @@ class Player(pygame.sprite.Sprite): #changes the image and can be moved. Use if 
         self.x = self.rect.x
         self.image = pygame.image.load(str(self.state) + ".png").convert()
         self.rect = self.image.get_rect()
-        self.rect.x = self.x + 1
+        self.rect.x = x
+        self.rect.y = y
+        self.image = pygame.transform.rotate(screen, self.dir)
+    def spinLeft(self, pressed):
+        self.dir -= 5
+
 
 
 pygame.init()
 
 screen = pygame.display.set_mode(size)
 bg = pygame.image.load("placeholder.png").convert()
-#player = Shuttle()
-vertices = 3
 
-
+player = Player()
 all_sprites_list = pygame.sprite.Group()
-#all_sprites_list.add(player)
+all_sprites_list.add(player)
+movespeed = 1  # Change this variable to alter the movement speed of the player.
 
 done = False
 while not done:
+    global movespeed, Player, angle
+    player = Player()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print "Click!"
-
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[pygame.K_w]:
+            y -= movespeed
+            print "w is pressed"
+    elif keys_pressed[pygame.K_s]:
+            y += movespeed
+            print "s is pressed"
+    elif keys_pressed[pygame.K_a]:
+            x -= movespeed
+            print "a is pressed"
+    elif keys_pressed[pygame.K_d]:
+            x += movespeed
+            print "d is pressed"
+    elif keys_pressed[pygame.K_q]:
+            player.spinLeft()
+            print "q is pressed"
 
     screen.blit(bg, (0, 0))
-    all_sprites_list.update()
+    all_sprites_list.update(keys_pressed)
     all_sprites_list.draw(screen)
 
     pygame.display.flip()
